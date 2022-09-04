207465d7e3757d83f8df1b740bf33c97ed5a151e
lokki931 <taurusr2d2@gmail.com>
2022-09-04 19:48:55 +0300

    add data&filter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { useState } from "react";
+import { Users } from "./store/users";
 import './App.css';
+import Table from './components/Table';
 
 function App() {
+  const [query, setQuery] = useState("");
+  const keys = ["first_name", "last_name", "email"];
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query.toLowerCase()))
+    );
+  };
   return (
-    <div className="App">
-      hello
+    <div className="app">
+      <input
+        className="search"
+        placeholder="Search..."
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+      />
+      {<Table data={search(Users)} />}
     </div>
   );
 }

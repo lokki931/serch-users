29391868914a5f5dbdba4cf45434968616f8c556
lokki931 <taurusr2d2@gmail.com>
2022-09-08 14:13:46 +0300

    add axios to fetching user, redact users db

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { useState } from "react";
-import { Users } from "./store/users";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import './App.css';
 import Table from './components/Table';
 
 function App() {
   const pages = 10;
+  const [users, setUsers] = useState([]);
   const [query, setQuery] = useState("");
   const [perPage, setPerPage] = useState(pages);
   const keys = ["first_name", "last_name", "email"];
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/users')
+      .then(res => setUsers(res.data))
+      .catch(e => console.log(e));
+  }, [])
+
+
   const search = (data) => {
     return data.filter((item) =>
       keys.some((key) => item[key].toLowerCase().includes(query))
@@ -20,10 +29,10 @@ function App() {
         placeholder="Search..."
         onChange={(e) => setQuery(e.target.value.toLowerCase())}
       />
-      {search(Users).length > 0 ?
-        <Table data={search(Users).splice(0, perPage)} /> :
+      {search(users).length > 0 ?
+        <Table data={search(users).splice(0, perPage)} /> :
         <p>nothing found</p>}
-      {search(Users).length > perPage ?
+      {search(users).length > perPage ?
         <div style={{ textAlign: 'center' }}>
           <button
             onClick={() => {
